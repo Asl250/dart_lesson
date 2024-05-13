f4677d0c4cc3880dbe4a88d1f7bdb80166598f5e
Asl250 <aslrahidov250@gmail.com>
2024-05-13 16:58:34 +0500

    commit

--- bin/dart_lesson.dart
+++ bin/dart_lesson.dart
@@ -1,3 +1,17 @@
 void main() {
+  String title = 'Hello, Dart!';
+  title = "nice to meet you!";
+
+  int number = 12212;
+  double rating = 5.0;
+
+  bool isHappy = true;
+  isHappy = false;
+
+
+  print(isHappy);
+  print(rating);
+  print(number);
+  print(title);
   print('Hello, World!');
 }
